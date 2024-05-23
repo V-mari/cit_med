d0641deac394f00f612fe42a8ba82d5c3723a9bf
V-mari <violettrujillo565@gmail.com>
2024-05-23 07:17:12 -0400

    instalacion, importacion, configuracion e inicializacion de Express

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+// Importación de Express
 const express = require('express');
+
+// Creación de una instancia de Express
 const app = express();
+
+// Importación del enrutador de Express
 const router = express.Router();
 
+// Puerto en el que escuchará el servidor
+const port = 3000;
 
-const port = 3000; // Puerto en el que escuchará el servidor
+
+// Inicialización del servidor Express
 app.listen(port, () => {
+    // Imprime un mensaje en la consola cuando el servidor se inicia correctamente
     console.log(`Servidor escuchando en el puerto ${port}`);
 });
